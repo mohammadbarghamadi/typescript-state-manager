5a1b041499e3be8e3befdf35b64cd0126bc4b111
Mohammad Barghamadi <mohammadbarghamadi@gmail.com>
2023-03-14 18:02:31 +0330

    this.error

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import Structure from "./template/Scaffold";
+import State from "./modules/State";
+import Container from "./modules/Container";
+import StatesTemplate from "./template/Template";
+
+const initial = () => {
+    const container = Container.instance
+    const template = StatesTemplate.instance
+
+    const input = document.querySelector('#user-input') as HTMLInputElement
+    const form = document.querySelector('form') as HTMLFormElement
+
+    form.addEventListener('submit', (e: SubmitEvent): void => {
+        e.preventDefault()
+        const text = input.value.trim()
+        if (!text.length) return
+
+        const itemId = container.list.length
+            ? parseInt(container.list[container.list.length - 1].id) + 1
+            : 1
+        const newItem = new State(true, 0, itemId.toString(), text, '')
+
+        container.add(newItem)
+
+        template.render(container)
+    })
+
+    container.load()
+    template.render(container)
+}
 
 const App = document.querySelector('#app') as HTMLDivElement
 App.append(Structure.building.render())
 
-document.addEventListener('DOMContentLoaded', () => {
-    
-})
+setTimeout(() => initial(), 100)
--- src/template/Template.ts
+++ src/template/Template.ts
@@ -3,23 +3,59 @@ import Container from "../modules/Container";
 
 export interface StatesTemplateInt {
     itemList: HTMLDivElement
-    clear():void
-    render():void
+    clear(): void
+    render(container: Container): void
 }
 
 export default class StatesTemplate implements StatesTemplateInt {
     static instance: StatesTemplate = new StatesTemplate()
     itemList: HTMLDivElement
-    private constructor () {
-        this.itemList = document.querySelector('#list-item') as HTMLDivElement
-        
+    private constructor() {
+        this.itemList = document.querySelector('#list-items') as HTMLDivElement
     }
 
     clear(): void {
-        
+        console.log(this.itemList)
+        this.itemList.innerHTML = ''
     }
 
-    render(): void {
-        
+    render(container: Container): void {
+        this.clear()
+
+        container.list.forEach(item => {
+            const element = document.createElement('div')
+            element.className = 'state'
+
+            const title = document.createElement('h3')
+            const description = document.createElement('p')
+            const increase = document.createElement('button')
+            const decrease = document.createElement('button')
+            const activator = document.createElement('input')
+
+            title.textContent = item.title
+            description.textContent = item.description
+
+            increase.textContent = 'Increase'
+            decrease.textContent = 'Decrease'
+
+            increase.className = 'inc'
+            decrease.className = 'dec'
+
+            increase.addEventListener('click', () => {
+                item.increase()
+                container.save()
+            })
+            decrease.addEventListener('click', () => {
+                item.decrease()
+                container.save()
+            })
+
+            activator.checked = true
+
+            element.append(title, description, increase, decrease, activator)
+
+            this.itemList.append(element)
+
+        })
     }
 }
